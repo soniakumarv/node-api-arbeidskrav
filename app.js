b7fc1a8fa7f355b6a3b2941453812825cffd9db4
Sonia Kumar <93277531+soniakumarv@users.noreply.github.com>
2022-11-16 20:33:16 +0100

    Finished all endpoints in API

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { env } = require("process");
 const bodyParser = require("body-parser");
 const res = require("express/lib/response");
 const sqlite3 = require("sqlite3");
+const { number } = require("prop-types");
 
 const PORT = 8080 || process.env.PORT;
 const app = express();
@@ -41,12 +42,10 @@ function createTables(newdb) {
         store text not null,
         location text not null,
         cardNumber int not null,
-        date DATE not null,
-        items text not null
+        date varchar not null,
+        items varchar not null
     );
 
-    insert into purchases (store, location, cardNumber, date, items)
-      values ('kiwi', 'Oslo', '2468', '2022-11-12', 'mushroom');
         `,
     (err) => {
       console.log(err);
@@ -68,12 +67,12 @@ app.post("/item", (req, res) => {
       item.id = items.length + 1;
       item.price = Number(item.price);
       items.push(item);
-      res.status(200).send("Item saved");
+      res.status(200).send("{'status': 'Item saved'}");
     } else {
-      res.status(200).send("Item is missing name or price");
+      res.status(200).send("{'status': 'Item is missing name or price'}");
     }
   } else {
-    res.status(200).send("There is no item");
+    res.status(200).send("{'status': 'There is no item'}");
   }
   console.log(items);
 });
@@ -94,26 +93,160 @@ app.post("/card", (req, res) => {
       card.date &&
       card.date.length > 0
     ) {
-      //1. hent alle items til lokal variabel
-      const allItems = [item];
-      //2. tøm items array
-      //3. lagre lokal items til database
+      const parsedItems = JSON.stringify(items);
 
       db.run(
-        "INSERT INTO purchases (store, location, cardNumber, date, items) VALUES ('" +
+        "INSERT INTO purchases (store, location, cardNumber, date, items) values ('" +
           card.store_name +
+          "', '" +
           card.store_location +
+          "', '" +
           card.card_number +
+          "', '" +
           card.date +
-          item.name +
+          "', '" +
+          parsedItems +
           "');"
       );
-      res.status(200).send("Purchase is OK");
+
+      items = [];
+      res.status(200).send("{'status': 'Purchase is OK'}");
     } else {
-      res.status(200).send("Missing data");
+      res.status(200).send("{'status': 'Missing data'}");
     }
   } else {
-    res.status(200).send("Missing data");
+    res.status(200).send("{'status': 'Missing data'}");
+  }
+});
+
+//GET/card
+app.get("/card/:card_number", (req, res) => {
+  console.log(req.params.card_number);
+  let data = [];
+  let card = req.params.card_number;
+  if (card && Number(card) && card.length == 4) {
+    db.serialize(() => {
+      db.each(
+        "SELECT items FROM purchases WHERE cardNumber = " + card + ";",
+        (err, row) => {
+          data.push(row);
+        },
+        () => {
+          res.send(data);
+        }
+      );
+    });
+  } else {
+    res.status(200).send("{'status': 'Missing number'}");
+  }
+});
+
+//GET/store_name
+app.get("/store/:store_name", (req, res) => {
+  let data = [];
+  let storeName = req.params.store_name;
+  if (storeName) {
+    db.serialize(() => {
+      db.each(
+        "SELECT * FROM purchases WHERE store = '" + storeName + "';",
+        (err, row) => {
+          data.push(row);
+        },
+        () => {
+          res.send(data);
+        }
+      );
+    });
+  } else {
+    res.status(200).send("{'status': 'Missing data'}");
+  }
+});
+
+//GET/store_location
+app.get("/location/:store_location", (req, res) => {
+  let data = [];
+  let storeLocation = req.params.store_location;
+  if (storeLocation) {
+    db.serialize(() => {
+      db.each(
+        "SELECT * FROM purchases WHERE location = '" + storeLocation + "';",
+        (err, row) => {
+          data.push(row);
+        },
+        () => {
+          res.send(data);
+        }
+      );
+    });
+  } else {
+    res.status(200).send("{'status': 'Missing data'}");
+  }
+});
+
+//GET/date
+app.get("/day/:date", (req, res) => {
+  let items = [];
+  let date = req.params.date;
+
+  if (date) {
+    db.serialize(() => {
+      db.each(
+        "SELECT items, date, cardNumber FROM purchases WHERE date = '" +
+          date +
+          "';",
+        (err, row) => {
+          items.push(row);
+        },
+        () => {
+          res.send(items);
+        }
+      );
+    });
+  } else {
+    res.status(200).send("{'status': 'Missing data'}");
+  }
+});
+
+//GET/month
+app.get("/month/:month_number/:year_number", (req, res) => {
+  let items = [];
+  let month = req.params.month;
+  let year = req.params.year;
+
+  if (
+    month &&
+    Number(month) &&
+    (month.length == 1 || month.length == 2) &&
+    year &&
+    Number(year) &&
+    year.length == 2
+  ) {
+    let date = month + "." + year;
+    db.serialize(() => {
+      db.each(
+        "SELECT items, date, cardNumber FROM purchases WHERE date LIKE '%" +
+          date +
+          "%';",
+        (err, row) => {
+          items.push(row);
+        },
+        () => {
+          res.send(items);
+        }
+      );
+    });
+  } else {
+    res.status(200).send("{'status': 'Missing data'}");
+  }
+});
+
+app.delete("/card/:card_number", (req, res) => {
+  let card = req.params.card_number;
+  if (card && Number(card) && card.length == 4) {
+    db.run("DELETE FROM purchases WHERE cardNumber = '" + card + "';");
+    res.status(200).send("{'status': 'ok'}");
+  } else {
+    res.status(200).send("{'status': 'Invalid card number'}");
   }
 });
 
